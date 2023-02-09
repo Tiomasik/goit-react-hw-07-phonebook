7470f61fab83e0b5c8049621466c9bcca997f56d
Artem Maslii <a.masliy@ukr.net>
2023-02-09 13:14:07 +0200

    delete useEffect at App and add in ContactList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,6 @@
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import { gettIsLoading, getError } from "redux/selectors";
-import { fetchAll } from "redux/operations";
 import Form from './Form/Form';
 import { ContactList } from './ContactList/ContactList'
 import Filter from './Filter/Filter'
@@ -13,13 +10,8 @@ import  Loader  from  '../components/Loader/Loader'
 
   
 const App = () => {
- const dispatch = useDispatch();
   const isLoading = useSelector(gettIsLoading);
   const error = useSelector(getError);
-
-  useEffect(() => {
-    dispatch(fetchAll());
-  }, [dispatch]);
 
   return (
     <AppBar>
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,15 +1,23 @@
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 import { getContacts, getFilter } from "redux/selectors";
 import ContactItem from '../ContactItem/ContactItem'
 import { List, Item, Info, Filter } from './ContactList.styled'
+import { fetchAll } from "redux/operations";
 
 export const ContactList = () => {
+    const dispatch = useDispatch();
     const contacts = useSelector(getContacts)
     const filter = useSelector(getFilter)
+
+    useEffect(() => {
+        dispatch(fetchAll());
+    }, [dispatch]);
     
     const getVisibleContacts = () => {
-    return contacts.filter(contact => contact.name.toLowerCase().includes(filter.trim().toLowerCase()))
+        return contacts.filter(contact => contact.name.toLowerCase().includes(filter.trim().toLowerCase()))
     }
         
     return (
